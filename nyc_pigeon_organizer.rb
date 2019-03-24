fac84581b8f0df29f58bbe8bc8dbb38bc4b9271a
Gino Desangles <blushingbox@yahoo.com>
2019-03-24 20:48:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  newList = {}
+  
+  data.each do |itemA,valA|
+    valA.each do |itemB,valB|
+      valB.each do |x|
+        newList[x] = {} if !newList[x]
+          if newList[x][itemA].nil?
+            newList[x][itemA] = [itemB.to_s]
+          else
+            newList[x][itemA] << itemB.to_s
+          end
+      end
+    end
+  end
+  
+  
+
+  return newList
 end
